5a414142ece3a8dc5e871ed5c19bd9966b0843c0
Jochen Klar <jochenklar@gmail.com>
2014-09-26 20:41:34 +0200

    cleanup for fetch

--- feeds/management/commands/fetch.py
+++ feeds/management/commands/fetch.py
@@ -1,7 +1,8 @@
 import datetime
-from django.utils.timezone import utc
+from django.utils.timezone import utc,now
 from django.core.management.base import BaseCommand, CommandError
-from feeds.models import Feed,Meta
+from feeds.models import Feed,Meta,Item
+from feeds.exceptions import FeedException
 
 class Command(BaseCommand):
     help = 'Fetches the items from all stored feeds.'
@@ -9,7 +10,15 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for feed in Feed.objects.all():
             print '    updating',feed
-            feed.fetchItems()
+
+            try:
+                feed.fetchItems()
+            except FeedException:
+                print '        skipping...'
+
+            # clean up everything older than 60 days but keep at least 30 Items
+            keep = feed.items.all()[:30].values_list("id",flat=True)
+            feed.items.filter(published__lt=(now() - datetime.timedelta(days=60))).exclude(pk__in=list(keep)).delete()
 
         # if everything went well set the updated field
         meta = Meta.load()
